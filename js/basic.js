0f0f4e4a325b227c164af04c39b7224c73bc55a9
Tony Garrett <tony.garrett@gmail.com>
2024-05-24 13:45:56 -0400

    header js complete

--- js/basic.js
+++ js/basic.js
@@ -1,1 +1,23 @@
 // Testing this out
+function changeCss () {
+  var headerElement = document.querySelector("header");
+  var navElement = document.querySelector(".main-nav");
+  var logoElement = document.querySelector(".main-logo");
+
+  if(this.scrollY > 50) {
+    headerElement.style.position = "Fixed";
+    headerElement.style.backgroundColor = "rgba(255,255,255,0.5)";
+    headerElement.style.padding = "1rem 10rem";
+    headerElement.style.minWidth = "100%";
+    logoElement.style.maxWidth = "10rem";
+    document.getElementById("logo").src="../img/logo-blue.png";
+  } else {
+    headerElement.style.position = "Static";
+    headerElement.style.backgroundColor = "transparent";
+    headerElement.style.padding = "1.8rem 12rem";
+    headerElement.style.minWidth = "100%";
+    logoElement.style.maxWidth = "16rem";
+    document.getElementById("logo").src="../img/logo-white.png";
+  }
+}
+window.addEventListener("scroll", changeCss , false);
