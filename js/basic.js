040dba82ab97653d6844a331838a44c95d860176
Anthony Garrett <tony.garrett@gmail.com>
2024-06-25 15:15:58 -0400

    Need JS for daily date

--- js/basic.js
+++ js/basic.js
@@ -1,7 +1,6 @@
 // Testing this out
 function changeCss () {
   var headerElement = document.querySelector("header");
-  var listElement = document.querySelector(".beerlist-content");
   var logoElement = document.querySelector(".main-logo");
   var featureList = document.querySelectorAll(".feature *");
 
@@ -11,14 +10,14 @@ function changeCss () {
     headerElement.style.padding = "1rem 10rem";
     headerElement.style.minWidth = "100%";
     logoElement.style.maxWidth = "10rem";
-    // document.getElementById("logo").src="../img/logo-blue.png";
+
   } else {
     headerElement.style.position = "Static";
     headerElement.style.backgroundColor = "transparent";
     headerElement.style.padding = "1.8rem 12rem";
     headerElement.style.minWidth = "100%";
     logoElement.style.maxWidth = "16rem";
-    // document.getElementById("logo").src="../img/logo-white.png";
+
   }
 
   if(this.scrollY > 300) {
